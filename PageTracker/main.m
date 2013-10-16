8941cd239c41e1cf4b6c89a6e37004e5cddebf26
Alexander von Below <Alex@vonBelow.Com>
2013-10-16 15:34:11 +0200

    Initial Commit

---
+++ PageTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PageTracker
+//
+//  Created by Alexander v. Below on 16.10.13.
+//  Copyright (c) 2013 Alexander v. Below. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PageTrackerTests/PageTrackerTests.m
@@ -0,0 +1,34 @@
+//
+//  PageTrackerTests.m
+//  PageTrackerTests
+//
+//  Created by Alexander v. Below on 16.10.13.
+//  Copyright (c) 2013 Alexander v. Below. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PageTrackerTests : XCTestCase
+
+@end
+
+@implementation PageTrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
